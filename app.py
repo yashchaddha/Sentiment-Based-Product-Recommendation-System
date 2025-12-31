fd36db5740b647ed89746c447f854c25de8e18cb
yashchaddha <yash.chaddha1997@gmail.com>
2025-12-31 16:56:27 +0530

    Prepare for Heroku deployment: add Procfile, update app.py for production, add gunicorn

--- app.py
+++ app.py
@@ -79,5 +79,6 @@ def internal_error(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
